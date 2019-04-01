aa57fda32d423904621c51714503ca39f2ed1f4c
Alejo Granero <alejogranero@gmail.com>
2019-04-01 15:19:34 -0300

    Hospitales - carga

--- routes/hospital.js
+++ routes/hospital.js
@@ -72,6 +72,34 @@ app.post('/', mdAutenticacion.verificaToken, function(req, res) {
     });
 });
 
+// ==========================================
+// Obtener Hospital por ID
+// ==========================================
+app.get('/:id', function(req, res) {
+    var id = req.params.id;
+    Hospital.findById(id)
+        .populate('usuario', 'nombre img email')
+        .exec(function(err, hospital) {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error al buscar hospital',
+                    errors: err
+                });
+            }
+            if (!hospital) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'El hospital con el id ' + id + 'no existe',
+                    errors: { message: 'No existe un hospital con ese ID' }
+                });
+            }
+            res.status(200).json({
+                ok: true,
+                hospital: hospital
+            });
+        });
+});
 
 
 // =====================================
